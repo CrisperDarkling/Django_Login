cdb4c1494b327fcce1a873c54c438bd18876c210
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-11-14 16:47:57 +0000

    14/11 16:47

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
+from Usercontacts.models import Contact
 
 # Create your views here.
+
+
 def get_index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated():
+        contacts = Contact.objects.filter(owner=request.user)
+    else:
+        contacts = []
+        
+    return render(request, "index.html", {"contacts": contacts})
     
 def logout(request):
     auth.logout(request)
-    return render(request, "index.html")
+    return redirect(get_index)
     
 def login(request):
     if request.method=="POST":
@@ -29,4 +37,25 @@ def login(request):
         form = UserLoginForm()
     
     return render(request, "login.html", {'form': form})
+    
+def register(request):
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            
+            user = auth.authenticate(username=form.cleaned_data["username"],
+                                        password=form.cleaned_data["password1"])
+                                        
+            if user is not None:
+                auth.login(request, user)
+                return redirect(get_index)
+                                        
+            
+    else:
+        form = UserRegistrationForm()
+        
+    return render(request, "register.html", {'form': form})
     
